13e578b0e3b207454db03ae87acce0eea0b9c586
KauaQT <kaua.teles@sptech.school>
2025-02-16 20:18:22 -0300

    Configurações iniciais springboot

---
+++ src/test/java/com/projetos/pratica3/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.pratica3.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
